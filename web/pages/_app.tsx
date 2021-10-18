285332f96699fb5e6dd99ab2becaa830359ccf23
phwt.smwt <phwt.smwt@gmail.com>
2021-10-18 17:17:49 +0700

    feat: edit layout

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,8 +1,14 @@
+import Head from "next/head";
 import "../styles/globals.css";
 
 const App = ({ Component, pageProps }) => {
   return (
     <div className="container">
+      <Head>
+        <title>Next.js on Azure</title>
+        <meta name="description" content="Next.js app deployed on Azure Static Web Apps" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Component {...pageProps} />
     </div>
   );
--- web/pages/admin/index.tsx
+++ web/pages/admin/index.tsx
@@ -1,5 +1,6 @@
 import withAuthorization from "../../containers/withAuthorization";
 import { getStaticPropsAllowedRoles } from "../../modules/Auth";
+import Head from 'next/head';
 
 export const getStaticProps = () => {
   return {
@@ -12,10 +13,12 @@ export const getStaticProps = () => {
 const AdminIndex = ({ user, allowedRoles }) => {
   return (
     <>
+    <Head>
+        <title>Next.js on Azure | Admin</title>
+      </Head>
       <h1>Manage Todo List</h1>
       <h5>Current user:</h5>
       <pre>{JSON.stringify(user, null, 2)}</pre>
-      <pre>{JSON.stringify(allowedRoles)}</pre>
     </>
   );
 };
